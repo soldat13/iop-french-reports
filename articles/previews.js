8b42aaa7584739f6ce14b90a97299f4c8b6b5788
Lucas P <lucas.panta@gmail.com>
2018-01-19 00:37:52 +0100

    init website

---
+++ articles/previews.js
@@ -0,0 +1,86 @@
+var previews = [
+  {
+    "url":"articles/20180114-rapport-transparence-3",
+    "datetime":"2018-01-14",
+    "datetext":"14 janvier 2018",
+    "title":"Rapport de transparence #3",
+    "subtitle":"Par Markus Maiwald - Fondateur du projet Libertaria",
+    "brief":"\
+        La transformation de Internet of People en une organisation décentralisée est en cours : actuellement, cette organisation comprend la société de développement IoP Ventures, les divisions de IoP, les chapitres IoP (membres à part entière de IoP) et la communauté IoP (actuellement définie comme tous les membres du chat Discord, plus tard définis comme des détenteurs des portefeuilles) qui coopèrent en tant qu'entités indépendantes travaillant ensemble vers les mêmes objectifs.\
+        <br><br>\
+        De tout cela, IoP Ventures est la seule entité qui opère légalement dans le \"vieux monde\", agissant en tant que société de développement commerciale pour donner vie à la crypto-monnaie. Les autres sont de\
+        [...]"
+  },
+  {
+    "url":"articles/20171221-disponibilite-titania-os-1-0-rc2-chapters",
+    "datetime":"2017-12-21",
+    "datetext":"21 décembre 2017",
+    "title":"Disponilité de Titania OS 1.0 RC2 pour les Chapters",
+    "subtitle":"",
+    "brief":""
+  },
+  {
+    "url":"articles/20171208-sortie-wallet-iop-version-6-1-0",
+    "datetime":"2017-12-08",
+    "datetext":"8 décembre 2017",
+    "title":"Disponibilité du wallet IoP en version 6.1.0",
+    "subtitle":"",
+    "brief":""
+  },
+  {
+    "url":"articles/20171104-mise-a-jour-majeure-iop-blockchain",
+    "datetime":"2017-11-04",
+    "datetext":"4 novembre 2017",
+    "title":"Mise à jour majeure de la blockchain",
+    "subtitle":"",
+    "brief":""
+  },
+  {
+    "url":"articles/20171009-rapport-transparence-2",
+    "datetime":"2017-10-09",
+    "datetext":"9 septembre 2017",
+    "title":"Rapport de transparence #2",
+    "subtitle":"",
+    "brief":""
+  },
+  {
+    "url":"./articles/20171008-developpement-rapport-transparence-1",
+    "datetime":"2017-10-08",
+    "datetext":"8 octobre 2017",
+    "title":"Développement & rapport de transparence #1",
+    "subtitle":"",
+    "brief":""
+  },
+  {
+    "url":"articles/20171005-video-people-power-4-communaute",
+    "datetime":"2017-10-05",
+    "datetext":"05 octobre 2017",
+    "title":"Vidéo - La communauté d'abord | People Power #4",
+    "subtitle":"",
+    "brief":""
+  },
+  {
+    "url":"articles/20170803-video-people-power-3-possedez-donnees",
+    "datetime":"2017-08-03",
+    "datetext":"03 août 2017",
+    "title":"Vidéo - Possédez vos données | People Power #3",
+    "subtitle":"",
+    "brief":""
+  },
+  {
+    "url":"articles/20170720-video-people-power-2-p2p-taxi-rencontres",
+    "datetime":"2017-07-20",
+    "datetext":"20 juillet 2017",
+    "title":"Vidéo - Cas d'utilisation P2P : taxi et rencontres | People Power #2",
+    "subtitle":"",
+    "brief":""
+  },
+  {
+    "url":"articles/20170706-video-people-power-1-internet-decentralise",
+    "datetime":"2017-07-06",
+    "datetext":"6 juillet 2017",
+    "title":"Vidéo - Un internet décentralisé | People Power #1",
+    "subtitle":"",
+    "brief":""
+  },
+];
